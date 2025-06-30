789e424ab1222176ca85ceea5bc8a8f42b6f7697
amircolgang <parham38385747@gmail.com>
2025-06-30 18:39:58 +0330

    Amircolgang: changes on 2025-06-30 18:39:57

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
+
+
 export default function Navbar() {
-  return (
+    const headerLink = [
+        {}
+    ]
+    return (
     <>
         <div className='w-[100vw] h-[70px] px-[20px]'>
             <div>
+                <ul>
 
+                </ul>
             </div>
             <div>
-                    
+
             </div>
         </div>
     </>
