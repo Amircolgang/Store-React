f081f003ec7c22a0ec04d178d0a3bef2673edb4b
amircolgang <parham38385747@gmail.com>
2025-06-30 18:08:01 +0330

    Amircolgang: changes on 2025-06-30 18:08:00

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
 import { Route , Routes } from "react-router";
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Womens",
-    },
-    {
-      id:5,
-      title: "Mens",
-    },
-  ]
+
   return (
     <>
-      <div className="categories-contianer">
-
-        {/* img */}
-        {
-          categories.map((category) => (
-            <div className="category-container">
-              <div className="category-body-container">
-                <h2>{category.title}</h2>
-                <p>Shop Now</p>
-              </div>
-            </div>
-          ))
-        }
-      </div>
+      <Route
     </>
   )
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
-
+const categories = [
+    {
+        id: 1,
+        title: "Hats",
+    },
+    {
+        id: 2,
+        title: "Jackets",
+    },
+    {
+        id: 3,
+        title: "Sneakers",
+    },
+    {
+        id: 4,
+        title: "Womens",
+    },
+    {
+        id: 5,
+        title: "Mens",
+    },
+]
 export default function Home() {
-  return (
-    <div>He</div>
-  )
+    return (
+        <>
+            <div className="categories-contianer">
+
+                {/* img */}
+                {
+                    categories.map((category) => (
+                        <div className="category-container">
+                            <div className="category-body-container">
+                                <h2>{category.title}</h2>
+                                <p>Shop Now</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
 }
