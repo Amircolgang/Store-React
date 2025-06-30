977dd44f86f12943b7de106f09646b33f21e5182
amircolgang <parham38385747@gmail.com>
2025-06-30 18:36:47 +0330

    Amircolgang: changes on 2025-06-30 18:36:46

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-        
+        <div className='w-[100vw]'>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
     </>
   )
 }
