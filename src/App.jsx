1aee2091ce0f16d295285564515608f9b3aedae5
amircolgang <parham38385747@gmail.com>
2025-06-30 18:10:10 +0330

    Amircolgang: changes on 2025-06-30 18:10:09

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route , Routes } from "react-router";
 import Home from "./pages/home/home";
+import Shops from "./pages/shops/shops";
 const App = () => {
 
   return (
     <>
       <Routes>
-        <Route path="/"element={Home} />
+        <Route path="/"element={<Home />} />
+        <Route path="/"element={<Shops />} />
       </Routes>
     </>
   )
--- src/pages/shops/shops.jsx
+++ src/pages/shops/shops.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Shops() {
   return (
-    <div>shops</div>
+    <>
+        This Page is Shop
+    </>
   )
 }
