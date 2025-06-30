1aa73e4b1d2ad080c32f5459b0985e7c4ffe2d0f
amircolgang <parham38385747@gmail.com>
2025-06-30 18:35:40 +0330

    Amircolgang: changes on 2025-06-30 18:35:40

--- src/components/offer/Offet.jsx
+++ src/components/offer/Offet.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Offet() {
   return (
     <>
-        <div className='w-[100vw] py-[12px] bg-blue-950 text-[#FFFFFF]'>
+        <div className='w-[100vw] py-[12px] bg-blue-950 flex items-center justify-center text-[#FFFFFF]'>
             <span>New here? Save 20% with code: YR24</span>
         </div>
     </>
