2d7528da62bee21219a684a746e6b0a84e0c3509
amircolgang <parham38385747@gmail.com>
2025-06-30 19:35:29 +0330

    Amircolgang: changes on 2025-06-30 19:35:28

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -32,9 +32,9 @@ export default function Navbar() {
     return (
         <>
             <div className='w-[100vw] h-[70px] px-[20px]'>
-                <div>
-                    <img className='w-[30px] h-[30px]' src="/public/" alt="توضیح تصویر" />
-                    <ul>
+                <div className='flex gap-[2.5rem]'>
+                    <img className='w-[30px] h-[30px]' src="/public/logo.png" alt="توضیح تصویر" />
+                    <ul className='flex '>
                         {
                             headerLink.map((links) => (
                                 <li>
