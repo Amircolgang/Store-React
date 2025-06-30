f3291cf1784a53195547537f0e2ea6f7a28a2206
amircolgang <parham38385747@gmail.com>
2025-06-30 18:41:03 +0330

    Amircolgang: changes on 2025-06-30 18:41:02

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,17 @@ import React from 'react'
 
 export default function Navbar() {
     const headerLink = [
-        {}
+        {   id : 1 ,
+            title : "" ,
+        } , 
+        {
+            id : 2 ,
+            title : "" ,
+        } ,
+        {
+            id
+            title : "" ,
+        }
     ]
     return (
     <>
